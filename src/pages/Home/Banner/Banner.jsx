09c9faf7bce73b911a85f776257c5bf899c6064f
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-27 23:56:59 +0600

    carousel-structure

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Banner = () => {
+	return (
+		<div>
+			<Carousel>
+				<div>
+					<img src='assets/1.jpeg' />
+					<p className='legend'>Legend 1</p>
+				</div>
+				<div>
+					<img src='assets/2.jpeg' />
+					<p className='legend'>Legend 2</p>
+				</div>
+				<div>
+					<img src='assets/3.jpeg' />
+					<p className='legend'>Legend 3</p>
+				</div>
+			</Carousel>
+		</div>
+	);
+};
+
+export default Banner;
