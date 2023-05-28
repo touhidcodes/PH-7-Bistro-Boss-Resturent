32202867da40f9c4c62a16c5e430149808046955
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-28 11:33:27 +0600

    order-section

--- src/pages/Home/OrderCategory/OrderCategory.jsx
+++ src/pages/Home/OrderCategory/OrderCategory.jsx
@@ -9,69 +9,76 @@ import slider2 from "../../../assets/home/slide2.jpg";
 import slider3 from "../../../assets/home/slide3.jpg";
 import slider4 from "../../../assets/home/slide4.jpg";
 import slider5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../shared/SectionTitle/SectionTitle";
 
 const OrderCategory = () => {
 	return (
-		<div className='px-40'>
-			<Swiper
-				slidesPerView={4}
-				spaceBetween={20}
-				pagination={{
-					clickable: true,
-				}}
-				modules={[Pagination]}
-				className='mySwiper mt-10 mb-10'
-			>
-				<SwiperSlide>
-					<img src={slider1} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Salads
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider2} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Pizzas
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider3} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Soups
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider4} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Deserts
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider5} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Salads
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider2} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Pizzas
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider3} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
-						Coffee
-					</h4>
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={slider4} alt='' />
-					<h4 className='uppercase text-3xl text-white -mt-20 text-center'>
-						Deserts
-					</h4>
-				</SwiperSlide>
-			</Swiper>
-		</div>
+		<section>
+			<SectionTitle
+				heading={"Order Online"}
+				subheading={"from 11.00 am to 10.00 pm"}
+			/>
+			<div className='px-40'>
+				<Swiper
+					slidesPerView={4}
+					spaceBetween={20}
+					pagination={{
+						clickable: true,
+					}}
+					modules={[Pagination]}
+					className='mySwiper mt-10 mb-10'
+				>
+					<SwiperSlide>
+						<img src={slider1} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Salads
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider2} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Pizzas
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider3} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Soups
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider4} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Deserts
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider5} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Salads
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider2} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Pizzas
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider3} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-28 text-center'>
+							Coffee
+						</h4>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src={slider4} alt='' />
+						<h4 className='uppercase text-3xl text-white -mt-20 text-center'>
+							Deserts
+						</h4>
+					</SwiperSlide>
+				</Swiper>
+			</div>
+		</section>
 	);
 };
 
---
+++ src/pages/shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SectionTitle = ({ subheading, heading }) => {
+	return (
+		<div className="text-center">
+			<p className="text-yellow-400 text-xl">...{subheading}...</p>
+			<h3 className="text-4xl border-y-2 border-zinc-300 w-fit px-10 py-2 mx-auto mt-3">{heading}</h3>
+		</div>
+	);
+};
+
+export default SectionTitle;
