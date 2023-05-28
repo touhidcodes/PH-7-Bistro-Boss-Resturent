69019a14748eb73b29cc4d8f69e5741903cd22bb
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-29 01:47:04 +0600

    parallax

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -10,11 +10,11 @@ const Featured = () => {
 				heading={"Featured From Menu"}
 				subheading={"Check it Out Now"}
 			/>
-			<div className='flex justify-center items-center px-28 py-8 featured-section'>
+			<div className='flex justify-center items-center px-28 py-8 featured-section bg-fixed mt-10 opacity-30'>
 				<div>
 					<img src={featured} alt='' className='h-48 w-[800px]' />
 				</div>
-				<div className='ml-10 space-y-2 pr-28 '>
+				<div className='ml-10 space-y-2 pr-28 font-semibold '>
 					<p className='uppercase'>March 20, 2023</p>
 					<p className='uppercase'>Where can i get some?</p>
 					<p>
@@ -23,6 +23,7 @@ const Featured = () => {
 						accusantium assumenda eum impedit. Vitae ratione eligendi, minima
 						aliquam autem blanditiis nemo?
 					</p>
+					<button className="btn btn-outline border-0 border-b-4">View Details</button>
 				</div>
 			</div>
 		</div>
