43e79f7c898d90b6cdc43d00202dc6106fdd6a5d
Touhidur Zaman <touhidcodes@gmail.com>
2023-06-03 01:56:42 +0600

    custom-hooks

--- src/pages/Home/MenuSection/MenuSection.jsx
+++ src/pages/Home/MenuSection/MenuSection.jsx
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../shared/SectionTitle/SectionTitle";
 import MenuItems from "../../shared/MenuItems/MenuItems";
+import useMenu from "../../../hooks/useMenu/useMenu";
 
 const MenuSection = () => {
-	const [menu, setMenu] = useState([]);
-	useEffect(() => {
-		fetch("menu.json")
-			.then((res) => res.json())
-			.then((data) => {
-				const popularItems = data.filter((item) => item.category === "popular");
-				setMenu(popularItems);
-			});
-	}, []);
-
+	const [menu] = useMenu();
+	const popularItems = menu.filter((item) => item.category === "popular");
 	return (
 		<section className='mb-10'>
 			<SectionTitle heading={"From Our Menu"} subheading={"Popular Menu"} />
 			<div className='grid grid-cols-2 gap-8 mt-10 '>
-				{menu.map((item) => (
+				{popularItems.map((item) => (
 					<MenuItems key={item._id} item={item} />
 				))}
 			</div>
