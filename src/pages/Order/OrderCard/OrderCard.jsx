ddcd0d039388cc400ee3572c5115f1ac7557cb01
Touhidur Zaman <touhidcodes@gmail.com>
2023-06-03 23:19:40 +0600

    reviews-data-load

--- src/pages/Order/OrderCard/OrderCard.jsx
+++ src/pages/Order/OrderCard/OrderCard.jsx
@@ -7,12 +7,16 @@ const OrderCard = ({ item }) => {
 			<figure>
 				<img src={image} alt='Shoes' />
 			</figure>
-			<p className="bg-slate-700 text-white font-semibold absolute right-5 top-5 px-3 py-1 rounded-xl">${price}</p>
+			<p className='bg-slate-700 text-white font-semibold absolute right-5 top-5 px-3 py-1 rounded-xl'>
+				${price}
+			</p>
 			<div className='card-body'>
 				<h2 className='card-title'>{name}</h2>
 				<p>{recipe}</p>
 				<div className='card-actions mx-auto mt-3'>
-					<button className='btn btn-primary'>Add to Cart</button>
+					<button className='btn btn-outline text-yellow-500 bg-slate-100 border-0 border-b-4'>
+						Add to Cart
+					</button>
 				</div>
 			</div>
 		</div>
